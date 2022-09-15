9c46e561f8c08154c476f3b502bdae6d90f36130
danielmuthama <danielmuthama23@gmail.com>
2022-09-15 04:53:07 +0300

    update files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a
+square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Private instance attribute: size
+Instantiation with size (no type/value verification)"""
+
+    def __init__(self, size):
+        self.__size = size
