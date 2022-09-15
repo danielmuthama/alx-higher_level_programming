d283b609c37071fdcdb90e67f437f63a9e88cfa7
danielmuthama <danielmuthama23@gmail.com>
2022-09-15 04:37:46 +0300

    update files

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""a function that divides 2 integers and prints the result."""
+
+
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except Exception:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
