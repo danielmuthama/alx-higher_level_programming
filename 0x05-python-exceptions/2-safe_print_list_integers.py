72ab4814be9034eb4d5ddd88bd6a97d377fb6e55
danielmuthama <danielmuthama23@gmail.com>
2022-09-15 04:37:21 +0300

    update files

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""a function that prints the first x
+elements of a list and only integers."""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for index in range(0, x):
+        try:
+            print("{:d}".format(my_list[index]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print('')
+    return count
