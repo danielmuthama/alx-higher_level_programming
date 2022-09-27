2d4e6a2bb3d5b8af936c09529fdcede24afa3688
danielmuthama <danielmuthama23@gmail.com>
2022-09-27 04:47:35 +0300

    update files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """method that raises an Exception with a message"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
