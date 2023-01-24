77f27192d30de682635b1bdff1239328fff2cdef
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2023-01-25 02:59:56 +0300

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,8 @@
+
 #!/usr/bin/node
-<<<<<<< HEAD
 
 exports.converter = function (base) {
   return function (num) {
     return num.toString(base);
-	};
-=======
-exports.converter = function (base) {
-  function convert (number) {
-    return number.toString(base);
-  }
-  return convert;
->>>>>>> 195949af5f2fd8f529bca1448ae08731cc6e2a99
+  };
 };
